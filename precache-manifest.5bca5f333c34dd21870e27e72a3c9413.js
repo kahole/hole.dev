da8bbef82d4b899ebe9701ae9712a0fe837e6372
Kristian Andersen Hole <khol@netlight.com>
2019-09-08 18:09:02 +0200

    Updates

---
+++ precache-manifest.5bca5f333c34dd21870e27e72a3c9413.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35f3bcfae48e0bb5e66d91d8329fdbf2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4e069929cf8bda66fe44",
+    "url": "/static/css/main.86bfa3b0.chunk.css"
+  },
+  {
+    "revision": "5372c74e30d9e22602a7",
+    "url": "/static/js/2.1cf07837.chunk.js"
+  },
+  {
+    "revision": "4e069929cf8bda66fe44",
+    "url": "/static/js/main.d79fe587.chunk.js"
+  },
+  {
+    "revision": "d8f9173cedb7fbe78861",
+    "url": "/static/js/runtime~main.78643d69.js"
+  }
+]);
