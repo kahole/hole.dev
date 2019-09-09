32c9749d9b1428a97cfb78304d8b1611f8e5d2da
Kristian Andersen Hole <khol@netlight.com>
2019-09-09 22:11:28 +0200

    adds arrow nav history to lisp repl

--- src/LispREPL.js
+++ src/LispREPL.js
@@ -1,33 +1,36 @@
 import React, { useState } from 'react';
 import './LispREPL.css';
 import {tokenize, parse, interpret} from './lisp.js'
+import {match} from 'egna';
 
 function LispREPL() {
 
   const sampleExpressions = [
-    // "> (+ 2 3)",
     "(set 'myFruit 'apple)",
     "(if (not (eq? (+ (* 10 2) 20) 400)) myFruit 'orange)",
     "(set 'pow2 (lambda (x) (* x x)))",
     "(pow2 5)",
     "(let (k 3) (+ k 6))",
-    // "> (set 'x (list (list 'a 5)))",
     "(cdr (assoc 'a (list (list 'a 5))))",
   ];
 
   const sampleHistory = [];
 
   sampleExpressions.forEach( h => {
-    sampleHistory.push("> " + h);
-    const result = interpret(parse(tokenize(h)), {})[0];
-    sampleHistory.push( typeof result === "function" ? {}.toString.call(result) : result.toString());
+    const command = h;
+    const output = interpret(parse(tokenize(command)), {})[0];
+    sampleHistory.push({command, output: typeof output === "function" ? {}.toString.call(output) : output.toString()});
   });
   
+  const [historyNavIndex, setHistoryNavIndex] = useState(0);
   const [history, setHistory] = useState(sampleHistory);
   const [currentLine, setCurrentLine] = useState("");
   
-  let historyList = history.map( (h,i) => {
-    return <p key={i} style={ i % 2 === 1 ? {color: 'green' } : {}} className="replHistoryItem">{h}</p>;
+  let historyList = history.map( ({command, output}, i) => {
+    return <span key={i}>
+      <p className="replHistoryItem">{"> " + command}</p>
+      <p style={ {color: 'green' } } className="replHistoryItem">{output}</p>
+    </span>
   });
 
   const onCurrentLineChange = e => {
@@ -42,8 +45,33 @@ function LispREPL() {
     } catch (e) {
       result = e.message;
     }
-    setHistory([...history, "> " + currentLine, result]);
+    setHistory([...history, {command: currentLine, output: result}]);
     setCurrentLine("");
+    setHistoryNavIndex(0);
+  };
+
+  const onKeyUp = e => {
+    const navDelta = match(
+      'ArrowUp', () => 1,
+      'ArrowDown', () => -1,
+      _ => 0
+    )(e.key);
+
+
+    if (navDelta !== 0) {
+      if (historyNavIndex+navDelta > history.length)
+        return;
+      
+      if (historyNavIndex+navDelta <= 0) {
+        setHistoryNavIndex(0);
+        setCurrentLine("");
+        return;
+      }
+
+      setHistoryNavIndex(historyNavIndex+navDelta);
+      const commandIndex = history.length - (historyNavIndex+navDelta);
+      setCurrentLine(history[commandIndex].command);
+    }
   };
 
   const onCurrentLineKeyPress = e => {
@@ -54,7 +82,10 @@ function LispREPL() {
   return (
     <div className="LispREPL">
       <div className="repl">
-        <input className="replInput" placeholder="> " value={currentLine} onChange={onCurrentLineChange} onKeyPress={onCurrentLineKeyPress} />
+        <div className="replInputWrapper">
+          <span className="replPrompt">&gt;</span>
+      <input className="replInput" value={currentLine} onChange={onCurrentLineChange} onKeyPress={onCurrentLineKeyPress} onKeyUp={onKeyUp} />
+        </div>
         { historyList.reverse() }
       </div>
     </div>
