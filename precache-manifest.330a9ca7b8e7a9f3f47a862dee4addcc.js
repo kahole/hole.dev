31d2ced38a76f14a389d4956675edfaedc71e045
kahole <kristian@hole.priv.no>
2020-03-17 16:08:05 +0100

    Updates

--- precache-manifest.330a9ca7b8e7a9f3f47a862dee4addcc.js
+++ precache-manifest.330a9ca7b8e7a9f3f47a862dee4addcc.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "88d430f8053ebef2d7492c921fe407e3",
+    "revision": "184aa63d3b857dc3dc6a97c7039214ba",
     "url": "/index.html"
   },
   {
-    "revision": "510ba06ad182ce4cdf89",
-    "url": "/static/css/main.f1c5b239.chunk.css"
+    "revision": "c2878f22e4f78140ebec",
+    "url": "/static/css/main.dd951941.chunk.css"
   },
   {
     "revision": "a6f018bc16a3824a23c2",
     "url": "/static/js/2.f0e03761.chunk.js"
   },
   {
-    "revision": "510ba06ad182ce4cdf89",
-    "url": "/static/js/main.5c87ddd3.chunk.js"
+    "revision": "c2878f22e4f78140ebec",
+    "url": "/static/js/main.52d555a9.chunk.js"
   },
   {
     "revision": "d8f9173cedb7fbe78861",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f1bca8ff8722a214265dcde3eaa7dd54.js"
+  "/precache-manifest.330a9ca7b8e7a9f3f47a862dee4addcc.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
